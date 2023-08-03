e2c09e68f41df02dd4b339445695a91566a44898
Rishi Gurjar <rishigurjar2000@gmail.com>
2023-08-03 16:55:25 -0700

    name and link

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -4,11 +4,18 @@ export const Navbar: FC = () => {
   return (
     <div className="flex h-[50px] sm:h-[60px] border-b border-neutral-300 py-2 px-2 sm:px-8 items-center justify-between">
       <div className="font-bold text-3xl flex items-center">
+      <a
+          className="ml-2 hover:opacity-100"
+          href="https://beyondterra.org/"
+        >
+          Beyond Terra |
+        </a>
+
         <a
           className="ml-2 hover:opacity-50"
-          href="https://beyondterra/terra"
+          href="https://terrabot-five.vercel.app/"
         >
-          Terra 🌲
+          Terra AI 🌲
         </a>
       </div>
       <p>This is an intiial prototype — links and information may be inaccurate. Please email <a href="mailto:rishi@beyondterra.org" style={{ color: 'green' }}>rishi@beyondterra.org</a> with any feedback.</p>
